41cf867b3233a4f2f6e129ce2640ffe9d8c70bb8
Davyd Kohut <davyd.kohut@gmail.com>
2025-05-17 20:20:30 +0300

    🧬

--- bib.c
+++ bib.c
@@ -1,6 +1,12 @@
+#include <arpa/inet.h>
+#include <liburing.h>
+#include <netinet/in.h>
+#include <poll.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #define п8 uint8_t
