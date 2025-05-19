93f60021e2c37f0f9a94b41bc535b74f694de5f4
Davyd Kohut <davyd.kohut@gmail.com>
2025-05-19 09:34:12 +0300

    🧬

---
+++ приклади/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
+
+mkdir -p tmp
+
+ціль tmp/інтернет_звʼязок.ll скомпілювати $SCRIPT_DIR/інтернет_звʼязок.ц
+clang -o tmp/інтернет_звʼязок tmp/інтернет_звʼязок.ll bib.c turbo.c -luring -lidn2
+
+ціль tmp/інтернет_обслуговувач.ll скомпілювати $SCRIPT_DIR/інтернет_обслуговувач.ц
+clang -o tmp/інтернет_обслуговувач tmp/інтернет_обслуговувач.ll bib.c turbo.c -luring -lidn2
