b31b7c5dde902f5c435991b5d15416cab747682e
Davyd Kohut <davyd.kohut@gmail.com>
2025-05-24 21:33:42 +0300

    🧬

--- приклади/build.sh
+++ приклади/build.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
 
 mkdir -p tmp
 
+ціль tmp/періодично.ll скомпілювати $SCRIPT_DIR/періодично.ц
+clang -o tmp/періодично tmp/періодично.ll bib.c turbo.c -luring -lidn2
+
 ціль tmp/відкласти.ll скомпілювати $SCRIPT_DIR/відкласти.ц
 clang -o tmp/відкласти tmp/відкласти.ll bib.c turbo.c -luring -lidn2
 
