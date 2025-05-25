ba8c7bf613f471f0d13c497ef668179e7a4e42a4
Davyd Kohut <davyd.kohut@gmail.com>
2025-05-25 23:37:58 +0300

    🧬

--- приклади/build.sh
+++ приклади/build.sh
@@ -6,14 +6,20 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
 
 mkdir -p tmp
 
+clang -c -o tmp/bib.o bib.c
+clang -c -o tmp/turbo.o turbo.c
+
+ціль tmp/прочитати_ввід.ll скомпілювати $SCRIPT_DIR/прочитати_ввід.ц
+clang -o tmp/прочитати_ввід tmp/прочитати_ввід.ll tmp/bib.o tmp/turbo.o -luring -lidn2 -lpthread -lreadline
+
 ціль tmp/періодично.ll скомпілювати $SCRIPT_DIR/періодично.ц
-clang -o tmp/періодично tmp/періодично.ll bib.c turbo.c -luring -lidn2
+clang -o tmp/періодично tmp/періодично.ll tmp/bib.o tmp/turbo.o -luring -lidn2 -lpthread -lreadline
 
 ціль tmp/відкласти.ll скомпілювати $SCRIPT_DIR/відкласти.ц
-clang -o tmp/відкласти tmp/відкласти.ll bib.c turbo.c -luring -lidn2
+clang -o tmp/відкласти tmp/відкласти.ll tmp/bib.o tmp/turbo.o -luring -lidn2 -lpthread -lreadline
 
 ціль tmp/інтернет_звʼязок.ll скомпілювати $SCRIPT_DIR/інтернет_звʼязок.ц
-clang -o tmp/інтернет_звʼязок tmp/інтернет_звʼязок.ll bib.c turbo.c -luring -lidn2
+clang -o tmp/інтернет_звʼязок tmp/інтернет_звʼязок.ll tmp/bib.o tmp/turbo.o -luring -lidn2 -lpthread -lreadline
 
 ціль tmp/інтернет_обслуговувач.ll скомпілювати $SCRIPT_DIR/інтернет_обслуговувач.ц
-clang -o tmp/інтернет_обслуговувач tmp/інтернет_обслуговувач.ll bib.c turbo.c -luring -lidn2
+clang -o tmp/інтернет_обслуговувач tmp/інтернет_обслуговувач.ll tmp/bib.o tmp/turbo.o -luring -lidn2 -lpthread -lreadline
